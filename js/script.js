e28b5134e4e14c11680b12dda1aa7d986d5d9138
Marcelo Comanduci <mcomanduci@gmail.com>
2024-07-27 11:41:31 -0300

    hero

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 "use strict";
 
 // Hero slider
-let count = 1;
-document.getElementById("radio1").checked = true;
+const hero = document.querySelector(".hero");
+const slides = document.querySelectorAll(".slide");
+
+let activeSlide = 0;
+
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slides[activeSlide].classList.add("active");
+}
 
 setInterval(() => {
-  count++;
-  if (count > 4) {
-    count = 1;
+  activeSlide++;
+  if (activeSlide === slides.length) {
+    activeSlide = 0;
   }
-  document.getElementById(`radio${count}`).checked = true;
-}, 5000);
+  setActiveSlide();
+  console.log(activeSlide);
+}, 3000);
