56cde84c3d5494e9ce4880dec44e7c5c7c0ffbc9
Marcelo Comanduci <mcomanduci@gmail.com>
2024-07-26 23:31:46 -0300

    slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Hero slider
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(() => {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
+  document.getElementById(`radio${count}`).checked = true;
+}, 5000);
