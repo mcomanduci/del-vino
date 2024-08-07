82e0f7204909571ca5f85878c272b71ea399ef2e
Marcelo Comanduci <mcomanduci@gmail.com>
2024-08-07 18:24:43 -0300

    wine-pg

--- js/script.js
+++ js/script.js
@@ -1,56 +1,60 @@
 "use strict";
 
 // Hero slider
-const hero = document.querySelector(".hero");
-const slides = document.querySelectorAll(".slide");
+if (document.querySelector(".hero")) {
+  const slides = document.querySelectorAll(".slide");
 
-let activeSlide = 0;
+  let activeSlide = 0;
 
-function setActiveSlide() {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
-  slides[activeSlide].classList.add("active");
-}
-
-setInterval(() => {
-  activeSlide++;
-  if (activeSlide === slides.length) {
-    activeSlide = 0;
+  function setActiveSlide() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slides[activeSlide].classList.add("active");
   }
-  setActiveSlide();
-}, 3000);
+
+  setInterval(() => {
+    activeSlide++;
+    if (activeSlide === slides.length) {
+      activeSlide = 0;
+    }
+    setActiveSlide();
+  }, 3000);
+}
 
 // Logo
 const logo = document.querySelector(".logo img");
 
 logo.addEventListener("mouseenter", () => {
-  logo.src = "img/logo-orig.svg";
-});
-
-logo.addEventListener("mouseleave", () => {
   logo.src = "img/logo-branco.svg";
 });
 
-// text-size
-const wineText = document.querySelector(".selection .title-container h2");
-const windowWidth = window.innerWidth;
-wineText.style.fontSize =
-  windowWidth < 1400 ? `${(window.innerWidth * 100) / 1400}%` : "100%";
-
-window.addEventListener("resize", (e) => {
-  const windowWidth = window.innerWidth;
-  if (windowWidth < 1420) {
-    wineText.style.fontSize = `${(windowWidth * 100) / 1400}%`;
-  } else {
-    wineText.style.fontSize = "100%";
-  }
+logo.addEventListener("mouseleave", () => {
+  logo.src = "img/logo-orig.svg";
 });
 
-// STORE TEST
-const storeContainer = document.querySelector(".store .wines-container");
+// text-size
+if (document.querySelector(".selection")) {
+  const wineText = document.querySelector(".selection .title-container h2");
+  const windowWidth = window.innerWidth;
+  wineText.style.fontSize =
+    windowWidth < 1400 ? `${(window.innerWidth * 100) / 1400}%` : "100%";
 
-const item = `
+  window.addEventListener("resize", (e) => {
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 1420) {
+      wineText.style.fontSize = `${(windowWidth * 100) / 1400}%`;
+    } else {
+      wineText.style.fontSize = "100%";
+    }
+  });
+}
+
+// STORE TEST
+if (document.querySelector(".store")) {
+  const storeContainer = document.querySelector(".store .wines-container");
+
+  const item = `
             <div class="store-img">
               <img src="img/wines/wine-1-small.png" alt="" />
             </div>
@@ -65,11 +69,12 @@ const item = `
             </div>
           `;
 
-for (let i = 0; i < 11; i++) {
-  const itemEl = document.createElement("div");
-  itemEl.classList.add("store-item");
-  itemEl.innerHTML = item;
-  storeContainer.appendChild(itemEl);
+  for (let i = 0; i < 11; i++) {
+    const itemEl = document.createElement("div");
+    itemEl.classList.add("store-item");
+    itemEl.innerHTML = item;
+    storeContainer.appendChild(itemEl);
+  }
 }
 
 // Go to top
